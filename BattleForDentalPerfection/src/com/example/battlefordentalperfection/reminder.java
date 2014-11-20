d6bcdc5974d6c97722c69d7a2fd513dbba5c79d4
JRBHcapteeth <bhernandez4492@yahoo.com>
2014-11-20 12:23:44 -0600

    change

--- BattleForDentalPerfection/src/com/example/battlefordentalperfection/reminder.java
+++ BattleForDentalPerfection/src/com/example/battlefordentalperfection/reminder.java
@@ -1,16 +1,19 @@
 package com.example.battlefordentalperfection;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import java.util.Calendar;
 
 import android.app.Activity;
 import android.app.AlarmManager;
 =======
 >>>>>>> origin/master
+=======
+import android.app.Activity;
+>>>>>>> parent of f0fb393... update to timer and reminder
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -24,6 +27,7 @@ import android.widget.Toast;
 
 <<<<<<< HEAD
 public class reminder extends Activity {
+<<<<<<< HEAD
     private TimePicker tpFirst = null; 
     private TimePicker tpSecond = null;
     private AlarmManager alarmMgr;
@@ -37,42 +41,41 @@ public class reminder extends Activity {
 public class reminder extends ActionBarActivity {
 
 >>>>>>> origin/master
+=======
+
+>>>>>>> parent of f0fb393... update to timer and reminder
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.reminder_home);
-      
-        Button btnNext = (Button)findViewById(R.id.btnBack);
-        Button btnTog = (Button) findViewById(R.id.tglRem);
+        setTitle("Reminder");
+
+        Button btnNext = (Button) findViewById(R.id.btnBack);
         Button btnSet = (Button) findViewById(R.id.btnSubmit);
-        alarmMgr = (AlarmManager)getSystemService(ALARM_SERVICE);
-    	Intent intent = new Intent(this, myAlarmReceiver.class);
-    	alarmIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
-         
-        //setCurrentTimeOnView();
         btnNext.setOnClickListener(buttonBackListener);
         btnSet.setOnClickListener(buttonSetListener);
         
-        
-           
-            
-    
-    }
-       
+        }
 
 <<<<<<< HEAD
         
+<<<<<<< HEAD
 =======
        
        	//TimePicker tpFirst = (TimePicker) findViewById(R.id.tpFirst);
         //TimePicker tpSecond = (TimePicker) findViewById(R.id.tpSecond);
 >>>>>>> origin/master
+=======
+        final   TimePicker tpFirst = (TimePicker) findViewById(R.id.tpFirst);
+        final   TimePicker tpSecond = (TimePicker) findViewById(R.id.tpSecond);
+>>>>>>> parent of f0fb393... update to timer and reminder
 
         public OnClickListener buttonSetListener = new OnClickListener() 
     	{
     		public void onClick(View v)
 <<<<<<< HEAD
     		{
+<<<<<<< HEAD
     			
     			
     			tpFirst = (TimePicker) findViewById(R.id.tpFirst);
@@ -88,14 +91,13 @@ public class reminder extends ActionBarActivity {
     		{/*
     			String strDateTime =" "+ tpFirst.getCurrentHour() + ":" + tpFirst.getCurrentMinute();
 >>>>>>> origin/master
+=======
+    			String strDateTime =" "+ tpFirst.getCurrentHour() + ":" + tpFirst.getCurrentMinute();
+>>>>>>> parent of f0fb393... update to timer and reminder
 
-             // Set the alarm to start at the users choice
-                Calendar calendar = Calendar.getInstance();
-                //timeMilli = calendar.getTimeInMillis();
-                calendar.set(Calendar.HOUR_OF_DAY, hour);
-                calendar.set(Calendar.MINUTE, minute);
-                timeMilli = calendar.getTimeInMillis();
+                Toast.makeText(reminder.this, "User has selected " + strDateTime, Toast.LENGTH_LONG).show();
 
+<<<<<<< HEAD
 <<<<<<< HEAD
                 // With setInexactRepeating(), you have to use one of the AlarmManager interval
                 // constants--in this case, AlarmManager.INTERVAL_DAY.
@@ -105,6 +107,9 @@ public class reminder extends ActionBarActivity {
 =======
                 finish();*/
 >>>>>>> origin/master
+=======
+                finish();
+>>>>>>> parent of f0fb393... update to timer and reminder
     		}
     	}; 
     	public OnClickListener buttonBackListener = new OnClickListener() 
