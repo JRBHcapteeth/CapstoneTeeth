f0fb3936fb55c7af64423f4368084b652099f61a
JRBHcapteeth <bhernandez4492@yahoo.com>
2014-11-20 12:09:34 -0600

    update to timer and reminder

--- BattleForDentalPerfection/src/com/example/battlefordentalperfection/reminder.java
+++ BattleForDentalPerfection/src/com/example/battlefordentalperfection/reminder.java
@@ -1,9 +1,13 @@
 package com.example.battlefordentalperfection;
 
+import java.util.Calendar;
+
 import android.app.Activity;
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,33 +19,68 @@ import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class reminder extends Activity {
-
+    private TimePicker tpFirst = null; 
+    private TimePicker tpSecond = null;
+    private AlarmManager alarmMgr;
+    private PendingIntent alarmIntent;
+    int hour;
+    int minute;
+    
+    
+    @Override 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.reminder_home);
-        setTitle("Reminder");
-
-        Button btnNext = (Button) findViewById(R.id.btnBack);
+      
+        Button btnNext = (Button)findViewById(R.id.btnBack);
+        Button btnTog = (Button) findViewById(R.id.tglRem);
         Button btnSet = (Button) findViewById(R.id.btnSubmit);
+        alarmMgr = (AlarmManager)getSystemService(ALARM_SERVICE);
+    	Intent intent = new Intent(this, myAlarmReceiver.class);
+    	alarmIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+         
+        //setCurrentTimeOnView();
         btnNext.setOnClickListener(buttonBackListener);
         btnSet.setOnClickListener(buttonSetListener);
         
-        }
+        
+           
+            
+    
+    }
+       
 
         
-        final   TimePicker tpFirst = (TimePicker) findViewById(R.id.tpFirst);
-        final   TimePicker tpSecond = (TimePicker) findViewById(R.id.tpSecond);
 
         public OnClickListener buttonSetListener = new OnClickListener() 
     	{
     		public void onClick(View v)
     		{
-    			String strDateTime =" "+ tpFirst.getCurrentHour() + ":" + tpFirst.getCurrentMinute();
+    			
+    			
+    			tpFirst = (TimePicker) findViewById(R.id.tpFirst);
+    			int minute;
+    			int hour;
+    			long timeMilli;
+    			minute = tpFirst.getCurrentMinute();
+    			hour = tpFirst.getCurrentHour();
+    			
+    			
+               // Toast.makeText(reminder.this, "User has selected " + hour + " " + minute, Toast.LENGTH_LONG).show();
 
-                Toast.makeText(reminder.this, "User has selected " + strDateTime, Toast.LENGTH_LONG).show();
+             // Set the alarm to start at the users choice
+                Calendar calendar = Calendar.getInstance();
+                //timeMilli = calendar.getTimeInMillis();
+                calendar.set(Calendar.HOUR_OF_DAY, hour);
+                calendar.set(Calendar.MINUTE, minute);
+                timeMilli = calendar.getTimeInMillis();
 
-                finish();
+                // With setInexactRepeating(), you have to use one of the AlarmManager interval
+                // constants--in this case, AlarmManager.INTERVAL_DAY.
+                alarmMgr.setInexactRepeating(AlarmManager.RTC_WAKEUP, timeMilli,
+                		                     AlarmManager.INTERVAL_DAY, alarmIntent);
+                
     		}
     	};
     	public OnClickListener buttonBackListener = new OnClickListener() 
--- BattleForDentalPerfection/src/com/example/battlefordentalperfection/timer.java
+++ BattleForDentalPerfection/src/com/example/battlefordentalperfection/timer.java
@@ -1,5 +1,86 @@
 package com.example.battlefordentalperfection;
 
-public class timer {
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class timer extends Activity {
+
+	private Button startButton;
+	private Button pauseButton;
+	
+	private TextView timerValue;
+	
+	private long startTime = 0L;
+	
+	private Handler customHandler = new Handler();
+	
+	long timeInMilliseconds = 0L;
+	long timeSwapBuff = 0L;
+	long updatedTime = 0L;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.timer);
+
+		timerValue = (TextView) findViewById(R.id.timerValue);
+
+		startButton = (Button) findViewById(R.id.startButton);
+		
+		startButton.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View view) {
+				startTime = SystemClock.uptimeMillis();
+				startButton.setClickable(false);
+				pauseButton.setClickable(true);
+				customHandler.postDelayed(updateTimerThread, 0);
+
+			}
+		});
+
+		pauseButton = (Button) findViewById(R.id.pauseButton);
+		
+		pauseButton.setOnClickListener(new View.OnClickListener() {
+		
+			public void onClick(View view) {
+			
+				timeSwapBuff += timeInMilliseconds;
+				startButton.setClickable(true);
+				pauseButton.setClickable(false);
+				customHandler.removeCallbacks(updateTimerThread);
+
+			}
+		});
+
+	}
+
+	private Runnable updateTimerThread = new Runnable() {
+
+		public void run() {
+			
+			timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+			
+			updatedTime = timeSwapBuff + timeInMilliseconds;
+
+			int secs = (int) (updatedTime / 1000);
+			int mins = secs / 60;
+			if(mins == 2)
+			{
+				Thread.currentThread().interrupt();
+			}
+			secs = secs % 60;
+			int milliseconds = (int) (updatedTime % 1000);
+			timerValue.setText("" + mins + ":"
+					+ String.format("%02d", secs) + ":"
+					+ String.format("%03d", milliseconds));
+			customHandler.postDelayed(this, 0);
+		}
+
+	};
 
 }
