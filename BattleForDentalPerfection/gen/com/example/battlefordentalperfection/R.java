887332db74621749bcb5709ec0bf67449093a30d
jhbh <jhbhcaps@gmail.com>
2014-11-23 15:28:05 -0600

    game with comments

--- BattleForDentalPerfection/gen/com/example/battlefordentalperfection/R.java
+++ BattleForDentalPerfection/gen/com/example/battlefordentalperfection/R.java
@@ -1090,14 +1090,15 @@ containing a value of this type.
         public static final int logogame=0x7f02005f;
         public static final int right=0x7f020060;
         public static final int startgamebg=0x7f020061;
-        public static final int vill1=0x7f020062;
-        public static final int vill2=0x7f020063;
-        public static final int vill3=0x7f020064;
-        public static final int vill4=0x7f020065;
-        public static final int vill5=0x7f020066;
-        public static final int vill6=0x7f020067;
-        public static final int vill7=0x7f020068;
-        public static final int vill8=0x7f020069;
+        public static final int testbg=0x7f020062;
+        public static final int vill1=0x7f020063;
+        public static final int vill2=0x7f020064;
+        public static final int vill3=0x7f020065;
+        public static final int vill4=0x7f020066;
+        public static final int vill5=0x7f020067;
+        public static final int vill6=0x7f020068;
+        public static final int vill7=0x7f020069;
+        public static final int vill8=0x7f02006a;
     }
     public static final class id {
         public static final int BiosLayout=0x7f09003c;
@@ -2823,7 +2824,7 @@ containing a value of this type.
          */
         public static final int[] MenuView = {
             0x010100ae, 0x0101012c, 0x0101012d, 0x0101012e,
-            0x0101012f, 0x01010130, 0x01010131, 0x01010438
+            0x0101012f, 0x01010130, 0x01010131, 0x01010435
         };
         /**
           <p>
