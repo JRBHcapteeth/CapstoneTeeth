f579d0e8dc9366cb3e8dc541c98390a2b854afe4
jhbh <jhbhcaps@gmail.com>
2014-12-10 09:10:02 -0600

    Revert "Revert "nearly final draft of app and doc""
    This reverts commit 4c830ccf3a26d7df798b3b50e5d307de996d5b73.

--- BattleForDentalPerfection/src/com/example/battlefordentalperfection/gameSplash.java
+++ BattleForDentalPerfection/src/com/example/battlefordentalperfection/gameSplash.java
@@ -40,6 +40,7 @@ public class gameSplash extends ActionBarActivity{
 		TextView textHighScoreEasy = (TextView) findViewById(R.id.textViewHighScoreEasy);
 		TextView textHighScoreMedium = (TextView) findViewById(R.id.textViewHighScoreMedium);
 		TextView textHighScoreHard = (TextView) findViewById(R.id.textViewHighScoreHard);
+		TextView textEarnCred = (TextView) findViewById(R.id.textViewCredits);
 		
 		if (sharedVars.getbIsEng())
 		{
@@ -62,6 +63,7 @@ public class gameSplash extends ActionBarActivity{
 			textHighScoreMedium.setText(message);
 			message = getString(R.string.textHighScoreHardEng)+" "+String.valueOf(sharedVars.getHighScore(2));
 			textHighScoreHard.setText(message);
+			textEarnCred.setText(getString(R.string.textEarnCredEng));
 		}
 		else
 		{
@@ -84,6 +86,7 @@ public class gameSplash extends ActionBarActivity{
 			textHighScoreMedium.setText(message);
 			message = getString(R.string.textHighScoreHardSpn)+" "+String.valueOf(sharedVars.getHighScore(2));
 			textHighScoreHard.setText(message);	
+			textEarnCred.setText(getString(R.string.textEarnCredSpn));
 		}
 	};
 	
